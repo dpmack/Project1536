36b67d9ad85b44f058261f98753412eee80d9638
David Mack <davidpmack@gmail.com>
2012-02-07 17:15:38 -0800

    Added ability to logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+include "helpers/auth.php";
+
+logout();
+
+?>
+<html>
+<head>
+<meta http-equiv="REFRESH" content="0;url=index.php">
+</head>
+<body>
+	You are being logged out.
+</body>
+</html>
