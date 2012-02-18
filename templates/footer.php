8d82eefa6c309223518bc5437371325eed64921b
Kevin McKeen <kevmckeenis@gmail.com>
2012-02-18 13:58:00 -0800

    Fixed up footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,25 @@
 <div id="footer">
-	<h3>Help?</h3>
+	<h3>Resource links</h3>
 	<ul>
-		<li><a href="#">about</a></li>
-		<li><a href="#">site map</a></li>
-		<li><a href="#">feedback</a></li>
-		<li><a href="#">d2l</a></li>
-		<li><a href="#">my.bcit.ca</a></li>
-		<li><a href="#">bcit.ca</a></li>
-		<li><a href="#">share.cas.bcit.ca</a></li>
-		<li><a href="#">msdnaa.bcit.ca</a></li>
+		<li><a href="about.php">About</a></li>
+		<li><a href="sitemap.php">Site map</a></li>
+		<li><a href="http://learn.bcit.ca" class="external">D2L</a></li>
+		<li><a href="http://my.bcit.ca" class="external">my.bcit.ca</a></li>
+		<li><a href="http://www.bcit.ca" class="external">bcit.ca</a></li>
+		<li><a href="http://share.cas.bcit.ca" class="external">share.cas.bcit.ca</a></li>
+		<li><a href="http://msdnaa.bcit.ca" class="external">MSDNAA</a></li>
 	</ul>
+	<p>
+		<a href="http://validator.w3.org/check?uri=referer"><img
+		src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+		</a>
+	</p>
+   
 </div>
 
 </div> <!-- /#wrapper_final -->
