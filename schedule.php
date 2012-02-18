b69c9e31b543a986de12b2908953d9ffe2a3cd2a
David Mack <davidpmack@gmail.com>
2012-02-18 13:31:45 -0800

    Troy's work on the schedule page

---
+++ schedule.php
@@ -0,0 +1,170 @@
+<?php
+include 'helpers/auth.php';
+include "templates/head.php";
+include "helpers/embededLogin.php";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+$headContent = '<link rel="stylesheet" type="text/css" href="css/schedule.css" />';
+if (!$GLOBALS['loggedIn'])
+{
+	$headContent .= '<meta http-equiv="Refresh" content="0; URL=login.php" />';
+}
+
+echo buildHead("Schedule",$headContent);
+?>
+<body>
+<?php
+include "templates/header.php";
+
+if (!$GLOBALS['loggedIn'])
+{	
+	echo "To view this page you must be logged in";
+	
+	include "templates/footer.php";
+	
+	if ($GLOBALS['sql_debug'] != 0)
+	{
+		include "templates/sqlDebug.php";
+	}
+	die();
+}
+?>
+
+<!-- !-->
+<div id="body">
+	<form id="scheduleForm">
+	
+		<select id="year">
+			<option selected value="default">Select Year </option>
+			<option value="Year1">Year 1</option>
+			<option value="Year2">Year 2</option>
+			<option value="Year3">Year 3</option>
+			<option value="Year4">Year 4</option>
+		</select> 
+		
+		<select id="term">
+			<option selected value="default">Select Term </option>
+			<option value="Year1">Term 1</option>
+			<option value="Year2">Term 2</option>
+		</select> 
+
+		<select id="Set">
+			<option selected value="default">Select Set </option>
+			<option value="Year1">Set A</option>
+			<option value="Year2">Set B</option>
+			<option value="Year1">Set C</option>
+			<option value="Year2">Set D</option>
+		</select>
+		
+		<input id="submit" type="submit" name="mysubmit" value="Update Schedule!"/>
+
+	</form>
+	
+
+	<table>
+	<tr>
+		<td></td>
+		<th>Monday</th>
+		<th>Tuesday</th>
+		<th>Wednesday</th>
+		<th>Thursday</th>
+		<th>Friday</th>
+	</tr>
+	<tr>
+		<th>8:30am</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>9:30am</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>10:30am</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>11:30am</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>12:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>1:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>2:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>3:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>4:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<th>5:30pm</th>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	</table>
+	
+</div>
+
+<!-- !-->
+<?php include "templates/footer.php"; ?>
+
+<?php
+if ($GLOBALS['sql_debug'] != 0)
+{
+	include "templates/sqlDebug.php";
+}
+?>
+</body>	
+</html>
