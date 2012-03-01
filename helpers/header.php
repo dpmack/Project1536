0c614d45a8354d4ebb8f91de0ecb5f2b435e3277
ubu <ubu@ubumini.(none)>
2012-02-29 19:31:15 -0800

    fixed links in nav to no-relative paths

--- helpers/header.php
+++ helpers/header.php
@@ -2,20 +2,20 @@
 <div class="wrapper"><div class="wrapper">
 	<ul id="quickLinks">
 		<?php if (!$GLOBALS['loggedIn']): ?>
-		<li><a href="login.php">log in</a></li>
-		<li><a href="register.php">join</a></li>
+		<li><a href="/login.php">log in</a></li>
+		<li><a href="/register.php">join</a></li>
 		<?php else: ?>
-		<li><a href="logout.php">log out</a></li>
-		<li><a href="landing.php">MYhub</a></li>
+		<li><a href="/logout.php">log out</a></li>
+		<li><a href="/landing.php">MYhub</a></li>
 		<?php endif; ?>
 	</ul>
 	<h1><a href="/">CSThub</a></h1>
 	<ul id="navigation">
-		<li><a href="index.php">Home</a></li>
-		<li><a href="forums.php">Forums</a></li>
-		<li><a href="faq.php">Help</a></li>
-		<li><a href="homework.php">Homework</a></li>
-		<li><a href="schedule.php">Schedule</a></li>
+		<li><a href="/index.php">Home</a></li>
+		<li><a href="/forums.php">Forums</a></li>
+		<li><a href="/faq.php">Help</a></li>
+		<li><a href="/homework.php">Homework</a></li>
+		<li><a href="/schedule.php">Schedule</a></li>
 	</ul>
 </div></div> <!-- /wrappers for header -->
 </div> <!-- /#header -->
