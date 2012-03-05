22e312121c98bc8cbe537db9429be22f61bf8476
David Mack <davidpmack@gmail.com>
2012-03-04 23:23:49 -0800

    Made sitemap look abit cleaner and point to correct url

--- sitemap.php
+++ sitemap.php
@@ -10,27 +10,34 @@ echo buildHead("Sitemap",$headContent);
 ?>
 <body>
 <?php include "helpers/header.php"; ?>
+<br />
+<br />
 <p>
 	<b>Non logged In content</b>
 </p>
-<a href="index.php">Homepage</a><br />
-<a href="login.php">Login Page</a><br />
-<a href="faq.php">FAQ</a><br />
-<a href="forums.php">Forums</a><br />
-<a href="topics.php">Topics</a><br />
-<a href="viewtopic.php">View Topic</a><br />
+<p>
+	<a href="index.php">Homepage</a><br />
+	<a href="login.php">Login Page</a><br />
+	<a href="faq.php">FAQ</a><br />
+	<a href="forums.php">Forums</a><br />
+	<a href="topics.php">Topics</a><br />
+	<a href="viewtopic.php">View Topic</a><br />
+</p>
 <p>
 	<b>Logged In content</b>
 </p>
-<a href="landing.php">Temporary Landing page</a><br />
-<a href="homework.php">Homework</a><br />
-<a href="schedule.php">Schedule</a><br />
+<p>
+	<a href="landing.php">Temporary Landing page</a><br />
+	<a href="homework.php">Homework</a><br />
+	<a href="schedule.php">Schedule</a><br />
+</p>
 <p>
 	<b>Logged In super user content (Note these are only displayed for marker convenace)</b>
 </p>
-<a href="admin/admin.php">General Admin</a><br />
-<a href="admin/addHomework.php">Add Homework</a><br />
-
+<p>
+	<a href="admin.php">General Admin</a><br />
+	<a href="addHomework.php">Add Homework</a><br />
+</p>
 <?php include "helpers/footer.php"; ?>
 
 <?php
