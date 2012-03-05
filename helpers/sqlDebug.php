0dfcd69090ce119404d9180217abda5e52448688
David Mack <davidpmack@gmail.com>
2012-03-04 22:55:17 -0800

    Fixed so fits on page properly

--- helpers/sqlDebug.php
+++ helpers/sqlDebug.php
@@ -1,6 +1,6 @@
 <div id="sqlDebugToggle" style="position: fixed; top: 0; right: 0; color: red; width: auto;" onclick="toggleSQLDebug()">SQL</div>
 <div id="sqlDebugDisplay" style="display:none; border: solid 1px; position: fixed; 
-	top:20px; right:10px; background: white; padding: 10px;"><?php 
+	top:20px; right:10px; background: white; padding: 10px; width: auto"><?php 
 if ($GLOBALS['sql_debug_buffer'])
 {
 	echo str_replace("\n","<br />", $GLOBALS['sql_debug_buffer']);
