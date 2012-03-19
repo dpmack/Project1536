fd5b7f1e1307973237fe103178898f8333245d80
David Mack <davidpmack@gmail.com>
2012-03-19 01:08:56 -0700

    Shows news posts and recent activity

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 include "helpers/auth.php";
 include "helpers/head.php";
 include "helpers/embededLogin.php";
+
+$activity = getRecentActivity();
+$news = getNews();
+
+$dateFormat = "d/m/Y - g:ia";
 ?>
 
 <?php echo buildHead("Home"); ?>
@@ -9,63 +14,32 @@ include "helpers/embededLogin.php";
 
 <?php include "helpers/header.php"; ?>
 
-
-
-
 	<div class="groupableSections">
-		<div class="section">
-			<h2>Homework</h2>
-			<ul>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-			</ul>
-		</div> <!-- /.section -->
-
 		<div class="section">
 			<h2>Recent Forum Activity</h2>
 			<ul>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
-				<li><a href="#">[2012.05.01] Item - Thing</a></li>
+				<?php
+				foreach ($activity as $topic)
+				{
+					echo "<li><a href='thread.php?topicID=" . $topic["topicID"] . "'>[" . 
+					date($dateFormat, $topic["createdDate"]) . "] " . $topic["title"] . "</a></li>";
+				}
+				?>
 			</ul>
 		</div> <!-- /.section -->
 	</div> <!-- /.groupableSections -->
+	
+	<hr /><br />
 
-	<div class="groupableSections">
-		<div class="section">
-			<h2>News 1</h2>
-			<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-			</p>
-			<p>
-			It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-			</p>
-		</div>
-
-		<div class="section">
-			<h2>News 2</h2>
-			<p>
-			It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-			</p>
-		</div>
-	</div> <!-- /.groupableSections -->
-
-
-
-
-
-
+	<?php
+	foreach ($news as $newsPost)
+	{
+		echo "<div class='groupableSections'>
+		<h2>" . $newsPost["title"] . "</h2>
+		" . $newsPost["content"] . "
+		</div>";
+	}
+	?>
 
 <?php include "helpers/footer.php"; ?>
 
