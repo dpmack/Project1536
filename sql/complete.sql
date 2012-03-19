2ba0ec7981c1b9140bd5e2e034c1f037c690acce
David Mack <davidpmack@gmail.com>
2012-03-19 01:11:15 -0700

    database dump, designed for live server, to use on local machines change the first line

--- sql/complete.sql
+++ sql/complete.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `a5621243_staging` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `a5621243_staging`;
+USE `csthub`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -12,6 +11,10 @@ USE `a5621243_staging`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `set`
+--
+
 DROP TABLE IF EXISTS `set`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -23,10 +26,18 @@ CREATE TABLE `set` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `set`
+--
+
 LOCK TABLES `set` WRITE;
 /*!40000 ALTER TABLE `set` DISABLE KEYS */;
 /*!40000 ALTER TABLE `set` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `setCourseMapping`
+--
 
 DROP TABLE IF EXISTS `setCourseMapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -38,10 +49,18 @@ CREATE TABLE `setCourseMapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `setcoursemapping`
+--
+
 LOCK TABLES `setCourseMapping` WRITE;
 /*!40000 ALTER TABLE `setCourseMapping` DISABLE KEYS */;
 /*!40000 ALTER TABLE `setCourseMapping` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `topics`
+--
 
 DROP TABLE IF EXISTS `topics`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -54,14 +73,22 @@ CREATE TABLE `topics` (
   `isLocked` tinyint(1) NOT NULL DEFAULT '0',
   `isSticky` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`topicID`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `topics`
+--
 
 LOCK TABLES `topics` WRITE;
 /*!40000 ALTER TABLE `topics` DISABLE KEYS */;
-INSERT INTO `topics` VALUES (1,1,1,'CST is Awesome',0,0),(2,2,1,'Lunch Lan Party',0,0),(3,2,2,'Web Dev Midterm',0,0),(4,1,2,'BUSA Quiz',0,0);
+INSERT INTO `topics` VALUES (1,1,1,'CSTHub is live',0,0);
 /*!40000 ALTER TABLE `topics` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `accounts`
+--
 
 DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -76,14 +103,46 @@ CREATE TABLE `accounts` (
   `emailConfirmed` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`accountID`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,'David','Mack','A00802872',' ','$6$rounds=45489$e1dd1f44b02518c4$L4Chnr2LLAyYvep.N9ORsh852KFYBgIzW01q08N38VcIVEaBU6S8uX1NdFIlwOSCh2ht0n27mEJRo8mE2LQs11',1),(2,'Kevin','McKeen','A00778394','kevmckeenis@gmail.com','$6$rounds=45489$cb258fb32c66bce1$jMihwGA3UrsszJ8ERKd1JRC3xHuQWlIjshYGJ5N2zutq0HG6SVkvgEAMH01SvelQfNYMBQ7agg20SefbeqWU0/',1),(10,'David','Mack','A0023424389','davidpmack@gmail.com','$6$rounds=45489$bef6f8157ea9b13f$TvE3NEIMy0HrPTP6NxKPnkt2NytnqcgheJWVnoMU8N9v0WiMrcTcbrXlvKrQoDaQui6HeVUzPWnohMGpLiMxO/',1);
+INSERT INTO `accounts` VALUES (1,'David','Mack','A00802872','davidpmack@gmail.com','$6$rounds=45489$e1dd1f44b02518c4$L4Chnr2LLAyYvep.N9ORsh852KFYBgIzW01q08N38VcIVEaBU6S8uX1NdFIlwOSCh2ht0n27mEJRo8mE2LQs11',1),(2,'Kevin','McKeen','A00778394','kevmckeenis@gmail.com','$6$rounds=45489$cb258fb32c66bce1$jMihwGA3UrsszJ8ERKd1JRC3xHuQWlIjshYGJ5N2zutq0HG6SVkvgEAMH01SvelQfNYMBQ7agg20SefbeqWU0/',1);
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `accountsCoursesMapping`
+--
+
+DROP TABLE IF EXISTS `accountsCoursesMapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accountsCoursesMapping` (
+  `accountID` int(11) NOT NULL,
+  `courseID` int(11) NOT NULL,
+  PRIMARY KEY (`accountID`,`courseID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accountsCoursesMapping`
+--
+
+LOCK TABLES `accountsCoursesMapping` WRITE;
+/*!40000 ALTER TABLE `accountsCoursesMapping` DISABLE KEYS */;
+INSERT INTO `accountsCoursesMapping` VALUES (1,1),(1,2);
+/*!40000 ALTER TABLE `accountsCoursesMapping` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `whiteboards`
+--
 
 DROP TABLE IF EXISTS `whiteboards`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -96,11 +155,19 @@ CREATE TABLE `whiteboards` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `whiteboards`
+--
+
 LOCK TABLES `whiteboards` WRITE;
 /*!40000 ALTER TABLE `whiteboards` DISABLE KEYS */;
 INSERT INTO `whiteboards` VALUES (1,'ghjk',1);
 /*!40000 ALTER TABLE `whiteboards` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `forums`
+--
 
 DROP TABLE IF EXISTS `forums`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -112,11 +179,19 @@ CREATE TABLE `forums` (
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `forums`
+--
+
 LOCK TABLES `forums` WRITE;
 /*!40000 ALTER TABLE `forums` DISABLE KEYS */;
 INSERT INTO `forums` VALUES (1,'News'),(2,'Homework'),(3,'Courses'),(4,'Off-Topic');
 /*!40000 ALTER TABLE `forums` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `confirmationemails`
+--
 
 DROP TABLE IF EXISTS `confirmationEmails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -130,10 +205,18 @@ CREATE TABLE `confirmationEmails` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `confirmationemails`
+--
+
 LOCK TABLES `confirmationEmails` WRITE;
 /*!40000 ALTER TABLE `confirmationEmails` DISABLE KEYS */;
 /*!40000 ALTER TABLE `confirmationEmails` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
 
 DROP TABLE IF EXISTS `permissions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -145,11 +228,19 @@ CREATE TABLE `permissions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `permissions`
+--
+
 LOCK TABLES `permissions` WRITE;
 /*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
 INSERT INTO `permissions` VALUES (1,'START_TOPIC'),(2,'REPLY'),(3,'START_TOPIC_NEWS'),(4,'CREATE_FORUM'),(5,'DELETE_REPLY'),(6,'LOGIN'),(7,'CREATE_DEPARTMENT'),(8,'EDIT_DEPARTMENT'),(9,'CREATE_COURSE'),(10,'EDIT_COURSE'),(11,'CREATE_HOMEWORK'),(12,'EDIT_HOMEWORK'),(13,'HIDE_HOMEWORK'),(14,'DELETE_HOMEWORK'),(15,'SITE_ADMINISTRATION');
 /*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tickets`
+--
 
 DROP TABLE IF EXISTS `tickets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -163,11 +254,18 @@ CREATE TABLE `tickets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `tickets`
+--
+
 LOCK TABLES `tickets` WRITE;
 /*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-INSERT INTO `tickets` VALUES ('00511a198321f0a2dd2eaaed065446e0ebced708','e853586aa17ddcf88ce89eec13d0e66b833acb76',1331921321,2);
 /*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `errors`
+--
 
 DROP TABLE IF EXISTS `errors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -178,13 +276,21 @@ CREATE TABLE `errors` (
   `message` text COLLATE latin1_general_ci NOT NULL,
   `comment` text COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`errorID`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `errors`
+--
 
 LOCK TABLES `errors` WRITE;
 /*!40000 ALTER TABLE `errors` DISABLE KEYS */;
 /*!40000 ALTER TABLE `errors` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `accountsRolesMapping`
+--
 
 DROP TABLE IF EXISTS `accountsRolesMapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -196,11 +302,19 @@ CREATE TABLE `accountsRolesMapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `accountsRolesMapping`
+--
+
 LOCK TABLES `accountsRolesMapping` WRITE;
-/*!40000 ALTER TABLE `accountsrolesmapping` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accountsRolesMapping` DISABLE KEYS */;
 INSERT INTO `accountsRolesMapping` VALUES (1,2),(2,2);
-/*!40000 ALTER TABLE `accountsrolesmapping` ENABLE KEYS */;
+/*!40000 ALTER TABLE `accountsRolesMapping` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
 
 DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -212,11 +326,19 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `roles`
+--
+
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` VALUES (1,'Disabled'),(2,'Super User'),(3,'Teacher'),(4,'Student'),(5,'Prospective Student'),(6,'Set Rep'),(7,'Banned');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `homeworkAccountMapping`
+--
 
 DROP TABLE IF EXISTS `homeworkAccountMapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -228,11 +350,18 @@ CREATE TABLE `homeworkAccountMapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `homeworkAccountMapping`
+--
+
 LOCK TABLES `homeworkAccountMapping` WRITE;
 /*!40000 ALTER TABLE `homeworkAccountMapping` DISABLE KEYS */;
-INSERT INTO `homeworkAccountMapping` VALUES (1,1);
 /*!40000 ALTER TABLE `homeworkAccountMapping` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
 
 DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -244,31 +373,47 @@ CREATE TABLE `posts` (
   `content` text NOT NULL,
   `createdDate` int(11) NOT NULL,
   `modifiedDate` int(11) DEFAULT NULL,
-  PRIMARY KEY (`postID`,`accountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`postID`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,1,1,'Man this forum is epic!',1331930888,NULL),(2,2,1,'I know right?',1331930988,1331930989);
+INSERT INTO `posts` VALUES (1,1,1,'<p>\n	CSTHub is now live and ready to be used.\n</p>\n<p>\n	As the first release there are some areas of the site which wont be open to public yet.\n	However in the mean time the homework checklist and forums are operational.\n</p>\n<p>\n	We ask for the moment that only CST students in sets 1A, 1B and 1C sign up for the site.\n	We will be opening the site to all CST students soon.\n</p>\n<p>	\n	We hope that you can find our site useful.\n	Please give any feedback or suggestions to us either in person or to our email <a href=\"mailto:csthub@gmail.com\">csthub@gmail.com</a>.\n</p>\n<p>\n	Thank you,<br />\n	<br />\n	CSTHub Crew - David, Kevin, Troy, Jay and Jake.\n</p>',1332140738,NULL);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `accountsCoursesMapping`;
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `accountsCoursesMapping` (
-  `accountID` int(11) NOT NULL,
-  `courseID` int(11) NOT NULL,
-  PRIMARY KEY (`accountID`,`courseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `departments` (
+  `departmentID` int(11) NOT NULL AUTO_INCREMENT,
+  `departmentName` varchar(4) COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`departmentID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-LOCK TABLES `accountsCoursesMapping` WRITE;
-/*!40000 ALTER TABLE `accountsCoursesMapping` DISABLE KEYS */;
-INSERT INTO `accountsCoursesMapping` VALUES (1,1),(1,2);
-/*!40000 ALTER TABLE `accountsCoursesMapping` ENABLE KEYS */;
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'COMM'),(2,'COMP'),(3,'BUSA');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
 
 DROP TABLE IF EXISTS `courses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -282,14 +427,22 @@ CREATE TABLE `courses` (
   `courseURL` text COLLATE latin1_general_ci,
   `displayName` varchar(40) COLLATE latin1_general_ci DEFAULT NULL,
   PRIMARY KEY (`courseID`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,1536,2,'Web Development',NULL,NULL,NULL),(2,1510,2,'Programming Methods',NULL,NULL,NULL);
+INSERT INTO `courses` VALUES (1,1536,2,'Web Development',NULL,NULL,NULL),(2,1510,2,'Programming Methods',NULL,NULL,NULL),(3,1111,2,'Essential Skills for Computing',NULL,NULL,NULL),(4,1113,2,'Applied Mathematics',NULL,NULL,NULL),(5,1116,1,'Business Commincations',NULL,NULL,NULL),(6,1100,2,'Enhanced Learning Skills',NULL,NULL,NULL),(7,2720,3,'Business in a Networked Econ',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `rolesPermissionsMapping`
+--
 
 DROP TABLE IF EXISTS `rolesPermissionsMapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -301,27 +454,19 @@ CREATE TABLE `rolesPermissionsMapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `rolespermissionsmapping`
+--
+
 LOCK TABLES `rolesPermissionsMapping` WRITE;
 /*!40000 ALTER TABLE `rolesPermissionsMapping` DISABLE KEYS */;
 INSERT INTO `rolesPermissionsMapping` VALUES (2,6);
 /*!40000 ALTER TABLE `rolesPermissionsMapping` ENABLE KEYS */;
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `department`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `department` (
-  `departmentID` int(11) NOT NULL AUTO_INCREMENT,
-  `departmentName` varchar(4) COLLATE latin1_general_ci NOT NULL,
-  PRIMARY KEY (`departmentID`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `department` WRITE;
-/*!40000 ALTER TABLE `department` DISABLE KEYS */;
-INSERT INTO `department` VALUES (1,'COMM'),(2,'COMP'),(3,'BUSA');
-/*!40000 ALTER TABLE `department` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Table structure for table `whiteboardsAccountsMapping`
+--
 
 DROP TABLE IF EXISTS `whiteboardsAccountsMapping`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -334,10 +479,18 @@ CREATE TABLE `whiteboardsAccountsMapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-LOCK TABLES `whiteboardsaccountsmapping` WRITE;
+--
+-- Dumping data for table `whiteboardsAccountsMapping`
+--
+
+LOCK TABLES `whiteboardsAccountsMapping` WRITE;
 /*!40000 ALTER TABLE `whiteboardsAccountsMapping` DISABLE KEYS */;
 /*!40000 ALTER TABLE `whiteboardsAccountsMapping` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `homework`
+--
 
 DROP TABLE IF EXISTS `homework`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -345,15 +498,20 @@ DROP TABLE IF EXISTS `homework`;
 CREATE TABLE `homework` (
   `homeworkID` int(11) NOT NULL AUTO_INCREMENT,
   `courseID` int(11) NOT NULL,
-  `assignment` varchar(128) COLLATE latin1_general_ci NOT NULL,
+  `title` varchar(128) COLLATE latin1_general_ci NOT NULL,
   `dueDate` int(11) NOT NULL,
+  `description` text COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`homeworkID`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `homework`
+--
 
 LOCK TABLES `homework` WRITE;
 /*!40000 ALTER TABLE `homework` DISABLE KEYS */;
-INSERT INTO `homework` VALUES (1,2,'Assignment 1',1338342400),(2,1,'Milestone 4',1339564264),(3,2,'troy sucks',149872941);
+INSERT INTO `homework` VALUES (5,1,'Milestone 5',1332831599,'Serverside programming'),(4,6,'Journal',1332226799,'Activity Log');
 /*!40000 ALTER TABLE `homework` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -366,3 +524,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2012-03-19  0:58:20
