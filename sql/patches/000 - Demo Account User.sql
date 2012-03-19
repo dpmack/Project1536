39d779ff6d704f272e082cb6536994ebb125beaf
David Mack <davidpmack@gmail.com>
2012-03-19 16:22:53 -0700

    So that the account will have the correct ID

--- sql/patches/000 - Demo Account User.sql
+++ sql/patches/000 - Demo Account User.sql
@@ -1,3 +1,7 @@
 INSERT INTO accounts
-(accountID, firstName, lastName, username, email, password, emailConfirmed)
-VALUES (0, "Demo", "User", "A00000000", "csthub+demoaccount@gmail.com", "", 1);
+(firstName, lastName, username, email, password, emailConfirmed)
+VALUES ("Demo", "User", "A00000000", "csthub+demoaccount@gmail.com", "", 1);
+
+UPDATE accounts
+SET accountID = 0
+WHERE username = "A00000000";
