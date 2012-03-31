7d8a6e61555f88bfb49138a24492e64f69f71c4e
unknown <Jay@LentoCirno.(none)>
2012-03-31 11:52:32 -0700

    About page

--- about.php
+++ about.php
@@ -12,7 +12,33 @@ echo buildHead("About",$headContent);
 <?php include "helpers/header.php"; ?>
 
 <div class="section">
-	<h2>Coming soon!</h2>
+	<p>	
+		We are five students that found the current BCIT system difficult to navigate.
+		BCIT has Desire 2 Learn, share In/Out, and my.bcit, that are being used by all the professors.
+		This system has caused students to be confused and spend more time on jumping around multiple websites for their studies. 
+		Therefore, we made this website to bring all three platforms together, and ultimately help students study with ease. 
+		With Forums written from scratch students can discuss homework problems, organize study groups and find out
+		the latest on what's happening around campus.
+		Our navigator provides quick access to [multiple platforms such as] D2L, MyBCIT and MSDNAA.
+		
+		
+		An updated homework checklist will allow students to keep track of their homework and due dates, all in one place.
+	</p>
+	<h2>Features</h2>
+	<p>
+		Access to D2L
+		Access to MyBcit
+		Access to MSDNAA
+		News
+		Updated Homework Checklist
+		Course Forum
+		Off-Topic Forum
+	</p>
+	
+	<p>
+	
+	</p>
+	
 </div>
 
 <?php include "helpers/footer.php"; ?>
