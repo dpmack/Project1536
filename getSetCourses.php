1d27b6a6ab5e5f44c5822c9da3b55a7f7a5e6767
David Mack <davidpmack@gmail.com>
2012-04-02 11:43:47 -0700

    Permission should not be on this page

--- getSetCourses.php
+++ getSetCourses.php
@@ -6,12 +6,6 @@ if (!$GLOBALS['loggedIn'])
 	die("Not logged in");
 }
 
-if (!hasPermission("SITE_ADMINISTRATION"))
-{
-	include "./error/notauth.php";
-	die();
-}
-
 $setID = filter_input(INPUT_GET,"setID", FILTER_VALIDATE_INT);
 
 if ($setID !== null)
